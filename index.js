14fa5ab77235fd168445af1d11e51ee06a9392e3
Muhammad Yahiya <64965102+Yahiya166@users.noreply.github.com>
2021-09-22 13:54:06 +0500

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const app = express();
 const bd = require('body-parser');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 let authModel = require('./authschema');
+const { findOne } = require('./authschema');
 const port = 5000;
 // this is requirments for backend End
 
@@ -25,18 +27,61 @@ app.get('/', (req, res) => {
 });
 
 
-app.post('/singup', (req, res) => {
+app.post('/singup', async (req, res) => {
     // console.log(req.body)
 
-    let userCreate = new authModel({ email: req.body.email, password: req.body.password });
-    userCreate.save().then((response) => {
-        // console.log(response, 'response success');
-        res.status(200).send({result:response,message:'user create successfully'})
-    }).catch((error) => {
-        // console.log(error, 'err');
-        res.status(400).send({result:error,message:'user are not create'})
-    });
+    // Authentication First
+    var checkUser = await authModel.findOne({ email: req.body.email })
+
+    // res.send({ result: checkUser });
+
+    if (checkUser) {
+        res.status(200).send({ result: checkUser, message: "user already registored" })
+    } else {
+        // res.status(200).send({ message: "Yes You can singup" })
+
+        var hashPassword = await bcrypt.hash(req.body.password, 12);
+        // res.send({ result: hashPassword });
+
+        // Save Data in DB
+        let userCreate = new authModel({ email: req.body.email, password: hashPassword });
+        userCreate.save().then((response) => {
+            // console.log(response, 'response success');
+            res.status(200).send({ result: response, message: 'user create successfully' })
+        }).catch((error) => {
+            // console.log(error, 'err');
+            res.status(400).send({ result: error, message: 'user are not create' })
+        });
+
+    }
+
+
 })
+
+app.post('/signin', async (req, res) => {
+
+    // Authentication First
+    var checkUser = await authModel.findOne({ email: req.body.email })
+
+    // res.send({ result: checkUser });
+
+    if (checkUser) {
+        var checkPassword = await bcrypt.compare(req.body.password, checkUser.password);
+
+        // res.status(200).send(checkPassword);
+
+        if (checkPassword) {
+            res.status(200).send({ message: "User login Successfull" });
+        } else {
+            res.status(403).send({ message: "your password is incorrect" })
+        }
+
+    } else {
+        res.status(200).send({ message: "No User is Registored with this email" })
+    }
+})
+
+
 // This is api connection End
 
 
